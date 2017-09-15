ed0ae3a9d0e9dd207f08960970bd584c518f9c95
Hugo Sykes <hugo@denmeadfarm.co.uk>
2017-09-15 15:04:19 +0100

    Now able to save the list of students to the file students.csv

--- directory.rb
+++ directory.rb
@@ -29,6 +29,7 @@ def print_menu
   puts "Type the corresponding number to select that option:"
   puts "1. Input students"
   puts "2. Print out all the students and their corresponding cohort"
+  puts "3. Save students"
   puts "9. Exit"
 end
 
@@ -38,6 +39,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+  when "3"
+    save_students
   when "9"
     exit
   else
@@ -78,6 +81,17 @@ end
 
 def print_footer
   puts @students.count != 1 ? "Overall, we have #{@students.count} great students" : "Overall, we have #{@students.count} great student"
+end
+
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
 end
 
 
