7f81d49cfec29e1d89f8b45da3e94d7ef7ab489b
Hugo Sykes <hugo@denmeadfarm.co.uk>
2017-09-15 18:30:14 +0100

    Now able to load files by specifying them in the command line

--- directory.rb
+++ directory.rb
@@ -20,7 +20,7 @@ students = [
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
@@ -55,11 +55,11 @@ end
 def input_students
   puts "Please enter the names of the students"
   puts "Press enter twice after entering the last name"
-  name = gets.chomp
+  name = STDIN.gets.chomp
   while !name.empty?
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
-    name = gets.chomp
+    name = STDIN.gets.chomp
   end
 end
 
@@ -97,14 +97,28 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
-    student_data = line.split(",")
-    @students << {name: student_data[0], cohort: student_data[1].to_sym}
+    name, cohort = line.chomp.split(",")
+    @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
 end
+
+
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else
+    puts "Sorry, #{filename} doesn't exist"
+    exit
+  end
+end
+
 
 
 interactive_menu
