c527aecc2e16fdd9c0aa1977181602257bfd07e2
Hugo Sykes <hugo@denmeadfarm.co.uk>
2017-09-18 11:12:21 +0100

    Added remove_student function and cleaned up the code

--- directory.rb
+++ directory.rb
@@ -15,13 +15,14 @@ students = [
 =end
 
 @students = []
-
+@filename = ''
 
 def interactive_menu
   try_load_students
   loop do
     print_menu
     process(STDIN.gets.chomp)
+    save_students
   end
 end
 
@@ -30,8 +31,8 @@ def print_menu
   puts "Type the corresponding number to select that option:"
   puts "1. Input students"
   puts "2. Print out all the students and their corresponding cohort"
-  puts "3. Save students"
-  puts "4. Load students"
+  puts "3. Load students"
+  puts "4. Remove students"
   puts "9. Exit"
 end
 
@@ -42,9 +43,9 @@ def process(selection)
   when "2"
     show_students
   when "3"
-    save_students
-  when "4"
     load_students
+  when "4"
+    remove_student
   when "9"
     exit
   else
@@ -56,11 +57,11 @@ end
 def input_students
   puts "Please enter the names of the students"
   puts "Press enter twice after entering the last name"
-  name = STDIN.gets.chomp
+  name = STDIN.gets.chomp.capitalize
   while !name.empty?
     @students << {name: name, cohort: :november}
     puts "Now we have #{@students.count} students"
-    name = STDIN.gets.chomp
+    name = STDIN.gets.chomp.capitalize
   end
 end
 
@@ -108,14 +109,44 @@ def load_students(filename = "students.csv")
 end
 
 
+def remove_student
+  puts "Enter name of student you want to remove:"
+  name = STDIN.gets.chomp.capitalize
+  return if name.nil?
+  puts "Now enter the name of the file you want to remove the name from if you choose"
+  filename = STDIN.gets.chomp
+  if filename == ''
+    filename = "students.csv"
+  end
+  @students.each_with_index { |a,idx| @students.delete_at(idx) if a[:name] == name}
+  remove_line_from_csv(name,filename)
+end
+
+
+def remove_line_from_csv(name,filename)
+  file = File.open(filename, "r")
+  file2 = File.open("dummy.csv", "w")
+  file.readlines.each do |line|
+    next if line.include?(name)
+    file2.puts line
+  end
+  file.close
+  file2.close
+  File.delete(filename)
+  File.rename("dummy.csv", filename)
+end
+
+
 def try_load_students
-  filename = ARGV.first
-  return if filename.nil?
-  if File.exists?(filename)
-    load_students(filename)
-    puts "Loaded #{@students.count} from #{filename}"
+  @filename = ARGV.first
+  if @filename.nil?
+    @filename = "students.csv"
+    load_students(@filename)
+  elsif File.exists?(@filename)
+    load_students(@filename)
+    puts "Loaded #{@students.count} from #{@filename}"
   else
-    puts "Sorry, #{filename} doesn't exist"
+    puts "Sorry, \"#{@filename}\" doesn't exist"
     exit
   end
 end
